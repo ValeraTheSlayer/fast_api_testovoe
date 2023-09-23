63c6c47da076bd4abd2430fdd53421bfbab6d87e
Valerij Ezhov <ezhov.fzzz@gmail.com>
2023-09-23 16:30:40 +0600

    Проставил лоигирование в Ðе

--- app/api.py
+++ app/api.py
@@ -1,9 +1,17 @@
+import logging
+import logging.config
 import uuid
 
 from fastapi import FastAPI
 
 from app.models import TaskStatus, CalcTask
 from app.tasks import async_calculate
+
+from app.logging_config import LOGGING_CONFIG
+
+
+logging.config.dictConfig(LOGGING_CONFIG)
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
@@ -12,16 +20,20 @@ tasks = {}
 
 @app.post("/calculate/", response_model=TaskStatus)
 async def calculate(task: CalcTask):
+    logger.info(f"Получено расчетное задание: {task.model_dump()}")
     task_id = str(uuid.uuid4())
     tasks[task_id] = {"status": "pending", "task": task, "result": None}
+    logger.debug(f"Создана новая задача с идентификатором: {task_id}")
     return TaskStatus(id=task_id, status="pending", result="Not calculated yet")
 
 
 @app.get("/result/", response_model=TaskStatus)
 async def get_result(task_id: str):
+    logger.info(f"Получение результата для идентификатора задачи: {task_id}")
     task_data = tasks.get(task_id, {"status": "not_found", "result": None})
 
     if task_data["status"] == "not_found":
+        logger.warning(f"Зада с идентификатором {task_id} не найдена")
         return TaskStatus(id=task_id, status="not_found", result="Not found")
 
     if task_data["status"] == "pending":
@@ -30,14 +42,17 @@ async def get_result(task_id: str):
             result = await async_calculate(task.x, task.y, task.operator)
             task_data["status"] = "complete"
             task_data["result"] = result
+            logger.debug(f"Задача {task_id} завершена со следующим результатом: {result}")
         except Exception as e:
             task_data["status"] = "error"
             task_data["result"] = str(e)
+            logger.error(f"При обработки задачи произошла ошибка {task_id}: {str(e)}")
 
     return TaskStatus(id=task_id, status=task_data["status"], result=str(task_data["result"]))
 
 
 @app.get("/tasks/", response_model=list[TaskStatus])
 def get_tasks():
+    logger.info("Выборка всех задач")
     return [TaskStatus(id=task_id, status=task["status"], result=str(task["result"])) for task_id, task in
             tasks.items()]
--- app/tasks.py
+++ app/tasks.py
@@ -1,4 +1,9 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
 async def async_calculate(x, y, operator):
+    logger.info(f"Рассчитываю: {x} {operator} {y}")
     if operator == '+':
         return x + y
     elif operator == '-':
@@ -7,7 +12,9 @@ async def async_calculate(x, y, operator):
         return x * y
     elif operator == '/':
         if y == 0:
+            logger.error("Попытка деления на 0")
             raise ValueError("Division by zero")
         return x / y
     else:
+        logger.error(f"Невалидный оператор: {operator}")
         raise ValueError("Invalid operator")
